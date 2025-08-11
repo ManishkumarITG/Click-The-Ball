9dbc5b45a89e5f8e8a995680ac9823f05a2079d7
ManishkumarITG <manishkumar@itgeeks.com>
2025-08-11 17:48:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ let ball = document.querySelector('.ball');
 let score = document.querySelectorAll('span');
 let Time = document.querySelector('#time');
 let btn = document.querySelectorAll('button')
-let pop_up = document.querySelector('.prompt')
+let pop_up = document.querySelector('.prompt'),
+    count = 0;
+function scor() {
+    count++;
+    score[0].innerHTML = count;
+    ball.style.display = 'none';
+}
 function start() {
     let clock = 30;
-    btn[0].setAttribute('disabled', '')
+    btn[0].setAttribute('disabled', '');
     let tem = setInterval(() => {
-        ball.style.display = 'block'
+        ball.style.display = 'block';
         let min = 1, max = 270;
         let position_x = Math.floor(Math.random() * (max - min + 1)) + min;
         let position_y = Math.floor(Math.random() * (max - min + 1)) + min;
-        ball.style.left = `${position_x}px`
-        ball.style.top = `${position_y}px`
-        clock = --clock;
+        ball.style.left = `${position_x}px`;
+        ball.style.top = `${position_y}px`;
+        --clock;
         Time.innerHTML = ` Time :  00 : ${clock}`
     }, 1000);
 
@@ -22,13 +28,13 @@ function start() {
         btn[0].setAttribute('abled', '');
         ball.style.display = 'none'
         clearInterval(tem)
-        alert(`Game Over 
-Your Scor ${count}`)
+        alert(`Game Over
+Your Scor ${count}`);
     }, 30000)
 
 }
 function scor() {
     count++;
-    score[0].innerHTML = `${count}`
-    ball.style.display = 'none'
+    score[0].innerHTML = count;
+    ball.style.display = 'none';
 }
